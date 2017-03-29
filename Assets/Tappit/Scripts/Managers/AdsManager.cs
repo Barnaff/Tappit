e09f5946258392f824f7fdf4d31eb34ef062ee04
Barnaff <kobi.chariski@gmail.com>
2017-03-30 01:55:07 +0300

    added video ads

--- Assets/Tappit/Scripts/Managers/AdsManager.cs
+++ Assets/Tappit/Scripts/Managers/AdsManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Advertisements;
 
 public class AdsManager : Kobapps.Singleton<AdsManager> {
 
@@ -28,6 +29,71 @@ public class AdsManager : Kobapps.Singleton<AdsManager> {
         if (completionAction != null)
         {
             completionAction(true);
+        }
+
+        if (Advertisement.IsReady("rewardedVideo"))
+        {
+            var options = new ShowOptions { resultCallback = (result)=>
+            {
+                bool sucsess = false;
+                switch (result)
+                {
+                    case ShowResult.Finished:
+                            {
+                                Debug.Log("The ad was successfully shown.");
+                    //
+                    // YOUR CODE TO REWARD THE GAMER
+                    // Give coins etc.
+                                sucsess = true;
+                                break;
+                            }
+                    case ShowResult.Skipped:
+                            {
+                                Debug.Log("The ad was skipped before reaching the end.");
+                                break;
+                            }
+                        case ShowResult.Failed:
+                            {
+                                Debug.LogError("The ad failed to be shown.");
+                                break;
+                            }
+
+                            if (completionAction != null)
+                            {
+                                completionAction(true);
+                            }
+                    }
+            }
+            };
+            Advertisement.Show("rewardedVideo", options);
+        }
+    }
+
+    public void ShowRewardedAd()
+    {
+        if (Advertisement.IsReady("rewardedVideo"))
+        {
+            var options = new ShowOptions { resultCallback = HandleShowResult };
+            Advertisement.Show("rewardedVideo", options);
+        }
+    }
+
+    private void HandleShowResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                Debug.Log("The ad was successfully shown.");
+                //
+                // YOUR CODE TO REWARD THE GAMER
+                // Give coins etc.
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("The ad was skipped before reaching the end.");
+                break;
+            case ShowResult.Failed:
+                Debug.LogError("The ad failed to be shown.");
+                break;
         }
     }
 
